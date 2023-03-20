5c1424e6b7ff55db14263655771f1baa00b11f4e
Jerônimo <jeronimoaa@al.insper.edu.br>
2023-03-20 17:40:35 -0300

    Infraestrutura basica acelerador e volante

---
+++ firmware/src/acelerador.c
@@ -0,0 +1,8 @@
+#include <asf.h>
+
+// precisa definir as funcoes no acelerador.h
+
+short get_acelerador(uint32_t afec, uint32_t channel) {
+	// fazer a leitura do analogico aqui e retornar um valor correspondente de 0 a 255
+	return 0;
+}
